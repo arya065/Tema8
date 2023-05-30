3c60f35fc6c2637525e050f24441f536271c5e2b
Alexey <arya065@g.educaand.es>
2023-05-30 14:37:59 +0200

    sql code fin 1.2

--- final Db2.sql
+++ final Db2.sql
@@ -1,52 +1,50 @@
-drop database if exists `FinalProject`;
-create database `FinalProject`;
+drop database if exists finalproject;
+create database finalproject;
 use FinalProject;
 
-drop table if exists `Fabricante`;
-CREATE TABLE Fabricante (
-    `idFabricante` INT NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(30) NOT NULL,
-    PRIMARY KEY (`idFabricante`)
+drop table if exists fabricante;
+CREATE TABLE fabricante (
+    id_fabricante INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id_fabricante)
 );
 
-drop table if exists `Producto`;
-CREATE TABLE `Producto` (
-    `idProducto` INT NOT NULL AUTO_INCREMENT,
-    `precio` DOUBLE NOT NULL,
-    `nombre` VARCHAR(20) NOT NULL,
-    `idFabricante` INT NOT NULL,
-    PRIMARY KEY (`idProducto`),
-    CONSTRAINT `fkFabricante` FOREIGN KEY (`idFabricante`)
-        REFERENCES `Fabricante` (`idFabricante`)
+drop table if exists producto;
+CREATE TABLE producto (
+    id_producto INT NOT NULL AUTO_INCREMENT,
+    precio DOUBLE NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    id_fabricante INT UNIQUE,
+    PRIMARY KEY (id_producto),
+    FOREIGN KEY (id_fabricante)
+        REFERENCES fabricante (id_fabricante)
 );
 
-drop table if exists `Empresa`;
-CREATE TABLE `Empresa` (
-    `idEmpresa` INT NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(20) NOT NULL,
-    -- `idCliente` INT NOT NULL,
-    `idProducto` INT NOT NULL,
-    PRIMARY KEY (`idEmpresa`),
-    CONSTRAINT `fkProducto` FOREIGN KEY (`idProducto`)
-        REFERENCES `Producto` (`idProducto`)
-    -- CONSTRAINT `fkCliente` FOREIGN KEY (`idCliente`)
---         REFERENCES `Cliente` (`idCliente`)
+drop table if exists empresa;
+CREATE TABLE empresa (
+    id_empresa INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(20) NOT NULL,
+    id_producto INT,
+    PRIMARY KEY (id_empresa),
+    CONSTRAINT fk_producto FOREIGN KEY (id_producto)
+        REFERENCES producto (id_producto)
 );
 
-drop table if exists `Cliente`;
-CREATE TABLE `Cliente` (
-    `idCliente` INT NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(30) NOT NULL,
-    `tel` VARCHAR(9),
-    `idEmpresa` INT NOT NULL,
-    PRIMARY KEY (`idCliente`)
+drop table if exists cliente;
+CREATE TABLE cliente (
+    id_cliente INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    tel VARCHAR(9),
+    PRIMARY KEY (id_cliente)
 );
-CREATE TABLE `Cliente_Empresa` (
-    `idCliente` INT,
-    `idEmpresa` INT,
-    FOREIGN KEY (`idCliente`)
-        REFERENCES Cliente (`idCliente`),
-    FOREIGN KEY (`idEmpresa`)
-        REFERENCES Empresa (`idEmpresa`),
-    PRIMARY KEY (`idCliente` , `idEmpresa`)
+CREATE TABLE cliente_empresa (
+    id_cliente INT,
+    id_empresa INT,
+    FOREIGN KEY (id_cliente)
+        REFERENCES cliente (id_cliente),
+    FOREIGN KEY (id_empresa)
+        REFERENCES empresa (id_empresa),
+    PRIMARY KEY (id_cliente , id_empresa)
 );
+
+
